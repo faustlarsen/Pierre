fc75bc6c6ee414c96d68ab2708721cfcb68dd76c
Constantine <faustlarsen@gmail.com>
2021-01-15 10:46:16 -0700

    add flavor and treat models

---
+++ Pierre/Models/Flavor.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Pierre.Models
+{
+public class Flavor
+    {
+        public Flavor()
+        {
+            this.Treats = new HashSet<FlavorTreat>();
+        }
+        public int FlavorId { get; set; }
+        public string FlavorName { get; set; }
+        public virtual ICollection<FlavorTreat> Treats { get; set; }
+    }
+}
