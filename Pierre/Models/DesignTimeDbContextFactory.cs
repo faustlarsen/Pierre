8c6b76b4415c97878e616a6931f54d945da8b55f
Constantine <faustlarsen@gmail.com>
2021-01-15 11:01:48 -0700

    add account and home controllers

--- Pierre/Models/DesignTimeDbContextFactory.cs
+++ Pierre/Models/DesignTimeDbContextFactory.cs
@@ -5,22 +5,21 @@ using System.IO;
 
 namespace Pierre.Models
 {
-  public class PierreContextFactory : IDesignTimeDbContextFactory<PierreContext>
-  {
-
-    PierreContext IDesignTimeDbContextFactory<PierreContext>.CreateDbContext(string[] args)
+    public class PierreContextFactory : IDesignTimeDbContextFactory<PierreContext>
     {
-      IConfigurationRoot configuration = new ConfigurationBuilder()
-          .SetBasePath(Directory.GetCurrentDirectory())
-          .AddJsonFile("appsettings.json")
-          .Build();
+        PierreContext IDesignTimeDbContextFactory<PierreContext>.CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
 
-      var builder = new DbContextOptionsBuilder<PierreContext>();
-      var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var builder = new DbContextOptionsBuilder<PierreContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
 
-      builder.UseMySql(connectionString);
+            builder.UseMySql(connectionString);
 
-      return new PierreContext(builder.Options);
+            return new PierreContext(builder.Options);
+        }
     }
-  }
 }
--- Pierre/Models/PierreContext.cs
+++ Pierre/Models/PierreContext.cs
@@ -3,11 +3,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Pierre.Models
 {
-  public class PierreContext : IdentityDbContext<ApplicationUser>
-  {
-    public virtual DbSet<Flavor> Flavors { get; set; }
-    public DbSet<Treat> Treats { get; set; }
-    public DbSet<FlavorTreat> FlavorTreat { get; set; }
-    public PierreContext(DbContextOptions options) : base(options) { }
-  }
+    public class PierreContext : IdentityDbContext<ApplicationUser>
+    {
+        public virtual DbSet<Flavor> Flavors { get; set; }
+        public DbSet<Treat> Treats { get; set; }
+        public DbSet<FlavorTreat> FlavorTreat { get; set; }
+        public PierreContext(DbContextOptions options) : base(options) { }
+    }
 }
